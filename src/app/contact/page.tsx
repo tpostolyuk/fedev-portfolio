23054f020d90d2ee8220b0fd7f819587e10bc3fe
Taras Postoliuk <tpostoliuk@osdb.io>
2024-11-11 10:57:35 +0200

    chore(contact): refactor mapping social links

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,10 @@
-import { FaEnvelope, FaGithub, FaLinkedin } from "react-icons/fa";
-import { FaSquareUpwork } from "react-icons/fa6";
+"use client";
+
 import { Bebas_Neue } from "next/font/google";
-import { BiLogoDevTo } from "react-icons/bi";
+import { motion } from "framer-motion";
 import clsx from "clsx";
+
+import { SOCIAL_LINKS } from "@/constants/options";
 
 const bebasNeue = Bebas_Neue({
   subsets: ["latin"],
@@ -18,7 +20,9 @@ export default function Contact() {
           "dark:bg-slate-900 dark:border-b-white"
         )}
       >
-        <h1
+        <motion.h1
+          initial={{ scale: 0, opacity: 0 }}
+          animate={{ scale: 1, opacity: 1 }}
           className={clsx(
             bebasNeue.className,
             "text-center text-6xl",
@@ -26,7 +30,7 @@ export default function Contact() {
           )}
         >
           CONTACT
-        </h1>
+        </motion.h1>
       </section>
       <section
         className={clsx(
@@ -35,7 +39,12 @@ export default function Contact() {
         )}
       >
         <div className="flex flex-col justify-start items-start gap-8 max-w-2xl w-full">
-          <div className="border-b-2 border-b-slate-900">
+          <div
+            className={clsx(
+              "border-b-2 border-b-slate-900",
+              "dark:border-b-white"
+            )}
+          >
             <h2 className="text-4xl text-left font-medium">
               Get in touch with me via links on social media or send me an{" "}
               <a
@@ -49,71 +58,26 @@ export default function Contact() {
             </h2>
           </div>
           <ul className="grid grid-cols-3 items-start gap-8">
-            <li className="hover:underline group">
-              <a
-                href="https://github.com/tpostolyuk"
-                className="flex items-center gap-2"
-                target="_blank"
-              >
-                <FaGithub
-                  className="group-hover:text-slate-800 group-hover:rotate-6"
-                  size={32}
-                />
-                <span>Github</span>
-              </a>
-            </li>
-            <li className="hover:underline group">
-              <a
-                href="https://www.linkedin.com/in/taras-p-8199491a5/"
-                className="flex items-center gap-2"
-                target="_blank"
-              >
-                <FaLinkedin
-                  className="group-hover:text-slate-800 group-hover:rotate-6"
-                  size={32}
-                />
-                <span>LinkedIn</span>
-              </a>
-            </li>
-            <li className="hover:underline group">
-              <a
-                href="https://www.upwork.com/freelancers/~011e871456341347b4"
-                className="flex items-center gap-2"
-                target="_blank"
-              >
-                <FaSquareUpwork
-                  className="group-hover:text-slate-800 group-hover:rotate-6"
-                  size={32}
-                />
-                <span>Upwork</span>
-              </a>
-            </li>
-            <li className="hover:underline group">
-              <a
-                href="mailto:tpstlk.work@gmail.com"
-                className="flex items-center gap-2"
-                target="_blank"
-              >
-                <FaEnvelope
-                  className="group-hover:text-slate-800 group-hover:rotate-6"
-                  size={30}
-                />
-                <span>Email</span>
-              </a>
-            </li>
-            <li className="hover:underline group">
-              <a
-                className="flex items-center gap-2"
-                href="https://dev.to/tpostoliuk"
-                target="_blank"
-              >
-                <BiLogoDevTo
-                  className="group-hover:text-slate-800 group-hover:rotate-6"
-                  size={32}
-                />
-                <span>Dev Community</span>
-              </a>
-            </li>
+            {SOCIAL_LINKS.map(({ label, url, Icon }) => (
+              <li key={label} className="hover:underline group">
+                <a
+                  className="flex items-center gap-2"
+                  href={url}
+                  target="_blank"
+                >
+                  {
+                    <Icon
+                      className={clsx(
+                        "group-hover:text-slate-800 group-hover:rotate-6",
+                        "dark:group-hover:text-zinc-100"
+                      )}
+                      size={32}
+                    />
+                  }
+                  <span>{label}</span>
+                </a>
+              </li>
+            ))}
           </ul>
         </div>
       </section>
--- src/constants/options.ts
+++ src/constants/options.ts
@@ -1,1 +1,25 @@
+import { FaEnvelope, FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaSquareUpwork } from "react-icons/fa6";
+import { BiLogoDevTo } from "react-icons/bi";
+
 export const NAV_ITEMS = ["ABOUT", "SKILLS", "CONTACT"];
+
+export const SOCIAL_LINKS = [
+  { label: "Github", Icon: FaGithub, url: "https://github.com/tpostolyuk" },
+  {
+    label: "LinkedIn",
+    Icon: FaLinkedin,
+    url: "https://www.linkedin.com/in/taras-p-8199491a5/",
+  },
+  {
+    label: "Upwork",
+    Icon: FaSquareUpwork,
+    url: "https://www.upwork.com/freelancers/~011e871456341347b4",
+  },
+  { label: "Email", Icon: FaEnvelope, url: "mailto:tpstlk.work@gmail.com" },
+  {
+    label: "Dev Community",
+    Icon: BiLogoDevTo,
+    url: "https://dev.to/tpostoliuk",
+  },
+];
