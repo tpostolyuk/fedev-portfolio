62b14e034c676252dbf1bf0c174f7ccb61ef8af8
Taras Postoliuk <tpostoliuk@osdb.io>
2024-11-12 21:44:02 +0200

    refactor(skills): extract skills for mapping

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,23 +1,6 @@
-import { MdAnimation, MdDateRange } from "react-icons/md";
-import { RiNextjsFill } from "react-icons/ri";
-import { BsBricks } from "react-icons/bs";
-import { TbApi } from "react-icons/tb";
 import clsx from "clsx";
-import {
-  FaChartSimple,
-  FaAngular,
-  FaGitAlt,
-  FaHtml5,
-  FaReact,
-  FaCss3,
-} from "react-icons/fa6";
-import {
-  SiStyledcomponents,
-  SiTailwindcss,
-  SiTypescript,
-  SiRedux,
-} from "react-icons/si";
 
+import { SKILLS } from "@/constants/options";
 import { Title } from "@/components/title";
 
 export default function Contact() {
@@ -26,87 +9,51 @@ export default function Contact() {
       <Title title="SKILLS" />
       <section
         className={clsx(
-          "flex h-full flex-col items-center justify-start py-8",
+          "flex h-full flex-col items-center justify-start px-4 py-8",
           "dark:border-b-white dark:bg-slate-900 dark:text-white",
         )}
       >
         <div className="flex w-full max-w-2xl flex-col items-start justify-start gap-8">
           <section className="flex flex-col gap-8">
-            <h2 className="text-2xl font-semibold">
+            <h2 className={clsx("text-xl font-semibold", "md:text-2xl")}>
               Programming Languages And Technologies:
             </h2>
-            <ul className="flex list-none items-center gap-6">
-              <li className="flex items-center gap-2 border-b-[1px] border-b-slate-900">
-                <FaReact size={18} />
-                <span>JavaScript</span>
-              </li>
-              <li className="flex items-center gap-2 border-b-[1px] border-b-slate-900">
-                <FaHtml5 size={18} />
-                <span>HTML</span>
-              </li>
-              <li className="flex items-center gap-2 border-b-[1px] border-b-slate-900">
-                <FaCss3 size={18} />
-                <span>CSS</span>
-              </li>
-              <li className="flex items-center gap-2 border-b-[1px] border-b-slate-900">
-                <TbApi size={18} />
-                <span>RESTful API</span>
-              </li>
+            <ul
+              className={clsx(
+                "flex list-none flex-wrap items-center gap-6 text-sm",
+                "md:text-base",
+              )}
+            >
+              {SKILLS.technologies.map(({ label, Icon }) => (
+                <li
+                  className="flex items-center gap-2 border-b-[1px] border-b-slate-900"
+                  key={label}
+                >
+                  <Icon size={18} />
+                  <span>{label}</span>
+                </li>
+              ))}
             </ul>
           </section>
           <section className="flex flex-col gap-8">
-            <h2 className="text-2xl font-semibold">Framework and Libraries:</h2>
-            <ul className="flex list-none flex-wrap items-center gap-6">
-              <li className="flex items-center gap-2 border-b-[1px] border-b-slate-900">
-                <SiTypescript size={18} />
-                <span>TypeScript</span>
-              </li>
-              <li className="flex items-center gap-2 border-b-[1px] border-b-slate-900">
-                <FaReact size={18} />
-                <span>ReactJs</span>
-              </li>
-              <li className="flex items-center gap-2 border-b-[1px] border-b-slate-900">
-                <RiNextjsFill size={18} />
-                <span>NextJS</span>
-              </li>
-              <li className="flex items-center gap-2 border-b-[1px] border-b-slate-900">
-                <FaAngular size={18} />
-                <span>Angular</span>
-              </li>
-              <li className="flex items-center gap-2 border-b-[1px] border-b-slate-900">
-                <SiRedux size={18} />
-                <span>Redux</span>
-              </li>
-              <li className="flex items-center gap-2 border-b-[1px] border-b-slate-900">
-                <FaGitAlt size={18} />
-                <span>Git (GitHub, GitLab, BitBucket)</span>
-              </li>
-              <li className="flex items-center gap-2 border-b-[1px] border-b-slate-900">
-                <SiTailwindcss size={18} />
-                <span>TailwindCSS</span>
-              </li>
-              <li className="flex items-center gap-2 border-b-[1px] border-b-slate-900">
-                <SiStyledcomponents size={18} />
-                <span>styled-components</span>
-              </li>
-              <li className="flex items-center gap-2 border-b-[1px] border-b-slate-900">
-                <FaChartSimple size={18} />
-                <span>
-                  Charts (chart.js, d3, echarts, recharts, highcharts)
-                </span>
-              </li>
-              <li className="flex items-center gap-2 border-b-[1px] border-b-slate-900">
-                <MdDateRange size={18} />
-                <span>Dates (momentjs, datefns, dayjs, luxon)</span>
-              </li>
-              <li className="flex items-center gap-2 border-b-[1px] border-b-slate-900">
-                <BsBricks size={18} />
-                <span>UI Components (mui, antd, radix, shadcdn, chakra)</span>
-              </li>
-              <li className="flex items-center gap-2 border-b-[1px] border-b-slate-900">
-                <MdAnimation size={18} />
-                <span>Animations: (framer-motion, react-transition-group)</span>
-              </li>
+            <h2 className={clsx("font-semibold md:text-2xl", "text-xl")}>
+              Framework and Libraries:
+            </h2>
+            <ul
+              className={clsx(
+                "flex list-none flex-wrap items-center gap-6 text-sm",
+                "md:text-base",
+              )}
+            >
+              {SKILLS.libraries.map(({ label, Icon }) => (
+                <li
+                  className="flex items-center gap-2 border-b-[1px] border-b-slate-900"
+                  key={label}
+                >
+                  <Icon size={18} />
+                  <span>{label}</span>
+                </li>
+              ))}
             </ul>
           </section>
         </div>
--- src/constants/options.ts
+++ src/constants/options.ts
@@ -1,6 +1,25 @@
-import { FaLinkedin, FaEnvelope, FaGithub } from "react-icons/fa";
-import { FaSquareUpwork } from "react-icons/fa6";
+import { FaChartSimple, FaSquareUpwork } from "react-icons/fa6";
+import { MdAnimation, MdDateRange } from "react-icons/md";
+import { RiNextjsFill } from "react-icons/ri";
 import { BiLogoDevTo } from "react-icons/bi";
+import { BsBricks } from "react-icons/bs";
+import { TbApi } from "react-icons/tb";
+import {
+  FaLinkedin,
+  FaEnvelope,
+  FaGithub,
+  FaReact,
+  FaAngular,
+  FaGitAlt,
+  FaHtml5,
+  FaCss3,
+} from "react-icons/fa";
+import {
+  SiStyledcomponents,
+  SiTailwindcss,
+  SiTypescript,
+  SiRedux,
+} from "react-icons/si";
 
 export const NAV_ITEMS = ["ABOUT", "SKILLS", "CONTACT"];
 
@@ -23,3 +42,35 @@ export const SOCIAL_LINKS = [
     url: "https://dev.to/tpostoliuk",
   },
 ];
+
+export const SKILLS = {
+  technologies: [
+    { label: "JavaScript", Icon: FaReact },
+    { label: "HTML", Icon: FaHtml5 },
+    { label: "CSS", Icon: FaCss3 },
+    { label: "RESTful API", Icon: TbApi },
+  ],
+  libraries: [
+    { label: "TypeScript", Icon: SiTypescript },
+    { label: "ReactJs", Icon: FaReact },
+    { label: "NextJS", Icon: RiNextjsFill },
+    { label: "Angular", Icon: FaAngular },
+    { label: "Redux", Icon: SiRedux },
+    { label: "Git (GitHub, GitLab, BitBucket)", Icon: FaGitAlt },
+    { label: "TailwindCSS", Icon: SiTailwindcss },
+    { label: "styled-components", Icon: SiStyledcomponents },
+    {
+      label: "Charts (chart.js, d3, echarts, recharts, highcharts)",
+      Icon: FaChartSimple,
+    },
+    { label: "Dates (momentjs, datefns, dayjs, luxon)", Icon: MdDateRange },
+    {
+      label: "UI Components (mui, antd, radix, shadcdn, chakra)",
+      Icon: BsBricks,
+    },
+    {
+      label: "Animations: (framer-motion, react-transition-group)",
+      Icon: MdAnimation,
+    },
+  ],
+};
