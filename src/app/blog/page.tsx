01426b6c58bfdf57410e477497a1774275198d52
Taras Postoliuk <tpostoliuk@osdb.io>
2024-11-14 12:43:31 +0200

    chore(blog): dark bg for skeleton

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -16,14 +16,44 @@ export default function Blog() {
       >
         <div className="flex w-full max-w-2xl flex-col items-start justify-start gap-8">
           <div className="flex w-full flex-col gap-2">
-            <div className="h-5 w-full max-w-60 animate-pulse rounded-md bg-gray-300" />
-            <div className="h-3 w-full max-w-40 animate-pulse rounded-md bg-gray-300" />
-            <div className="h-40 w-full animate-pulse rounded-md bg-gray-300" />
+            <div
+              className={clsx(
+                "h-5 w-full max-w-60 animate-pulse rounded-md bg-gray-300",
+                "dark:bg-gray-200",
+              )}
+            />
+            <div
+              className={clsx(
+                "h-3 w-full max-w-40 animate-pulse rounded-md bg-gray-300",
+                "dark:bg-gray-200",
+              )}
+            />
+            <div
+              className={clsx(
+                "h-40 w-full animate-pulse rounded-md bg-gray-300",
+                "dark:bg-gray-200",
+              )}
+            />
           </div>
           <div className="flex w-full flex-col gap-2">
-            <div className="h-5 w-full max-w-60 animate-pulse rounded-md bg-gray-300" />
-            <div className="h-3 w-full max-w-40 animate-pulse rounded-md bg-gray-300" />
-            <div className="h-40 w-full animate-pulse rounded-md bg-gray-300" />
+            <div
+              className={clsx(
+                "h-5 w-full max-w-60 animate-pulse rounded-md bg-gray-300",
+                "dark:bg-gray-200",
+              )}
+            />
+            <div
+              className={clsx(
+                "h-3 w-full max-w-40 animate-pulse rounded-md bg-gray-300",
+                "dark:bg-gray-200",
+              )}
+            />
+            <div
+              className={clsx(
+                "h-40 w-full animate-pulse rounded-md bg-gray-300",
+                "dark:bg-gray-200",
+              )}
+            />
           </div>
         </div>
       </section>
