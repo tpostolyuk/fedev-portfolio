1276abbc0a71c1f8bc58c41d2e44a91b17ccc726
Taras Postoliuk <tpostoliuk@osdb.io>
2024-11-14 13:28:17 +0200

    chore(header): remove border right from logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -20,15 +20,15 @@ export const Header: FC = () => {
   return (
     <header
       className={clsx(
-        "sticky left-0 right-0 top-0 z-10 flex h-[65px] items-center gap-8 border-b-[1px] border-b-slate-900 bg-zinc-100 dark:bg-slate-900",
-        "dark:border-b-white",
+        "sticky left-0 right-0 top-0 z-10 flex h-[65px] items-center gap-8 border-y-[1px] border-y-slate-900 bg-zinc-100 dark:bg-slate-900",
+        "dark:border-y-white",
       )}
     >
       <Link href="/">
         <div
           className={clsx(
-            "flex h-8 max-w-fit items-center justify-center border-r-[1px] border-r-slate-900 p-8 font-semibold text-slate-900 md:text-xl",
-            "dark:border-r-white dark:bg-slate-900 dark:text-white",
+            "flex h-8 max-w-fit items-center justify-center p-6 font-semibold text-slate-900 md:text-[18px]",
+            "dark:bg-slate-900 dark:text-white",
             "cursor-pointer hover:underline",
             currentPath === "/" && "underline",
           )}
