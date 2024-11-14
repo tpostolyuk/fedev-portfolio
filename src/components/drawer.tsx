a4ef3010851ada1e23855ce2d459b190b6d2375e
Taras Postoliuk <tpostoliuk@osdb.io>
2024-11-14 13:16:16 +0200

    chore(drawer): dark mode for drawer, add theme toggle for mobile

--- src/components/drawer.tsx
+++ src/components/drawer.tsx
@@ -7,6 +7,7 @@ import { FC } from "react";
 import clsx from "clsx";
 
 import { NAV_ITEMS } from "@/constants/options";
+import { ThemeToggle } from "./theme-toggle";
 
 interface Props {
   onClose: () => void;
@@ -22,13 +23,32 @@ export const Drawer: FC<Props> = ({ onClose }) => {
     >
       <div
         onClick={(e) => e.stopPropagation()}
-        className="fixed bottom-0 right-0 top-0 z-10 w-[70%] bg-zinc-100 p-16"
+        className={clsx(
+          "fixed bottom-0 right-0 top-0 z-10 w-[70%] bg-zinc-100 p-16",
+          "dark:bg-slate-900",
+        )}
       >
         <div className="flex flex-col items-center gap-8">
           <button className="ml-auto" onClick={onClose}>
             <IoMdClose size={32} />
           </button>
-          <h2 className="text-3xl font-bold">FEDEV</h2>
+
+          <Link href="/">
+            <h2
+              className={clsx(
+                "text-3xl font-bold",
+                currentPath === "/" && "underline",
+                "dark:text-white",
+              )}
+            >
+              FEDEV
+            </h2>
+          </Link>
+          <div
+            className={clsx("rounded-md bg-slate-200 p-2", "dark:bg-slate-700")}
+          >
+            <ThemeToggle />
+          </div>
         </div>
         <nav className="flex h-full items-center justify-center">
           <ul
