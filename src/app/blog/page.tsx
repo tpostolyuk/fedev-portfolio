0b71542e964f692aad13c51afacb0444c8bdc903
Taras Postoliuk <tpostoliuk@osdb.io>
2024-11-16 18:33:43 +0200

    chore(blog): remove animate pulse from blog coming soon section

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -18,19 +18,19 @@ export default function Blog() {
           <div className="flex w-full flex-col gap-2">
             <div
               className={clsx(
-                "h-5 w-full max-w-60 animate-pulse rounded-md bg-gray-300",
+                "h-5 w-full max-w-60 rounded-md bg-gray-300",
                 "dark:bg-gray-200",
               )}
             />
             <div
               className={clsx(
-                "h-3 w-full max-w-40 animate-pulse rounded-md bg-gray-300",
+                "h-3 w-full max-w-40 rounded-md bg-gray-300",
                 "dark:bg-gray-200",
               )}
             />
             <div
               className={clsx(
-                "h-40 w-full animate-pulse rounded-md bg-gray-300",
+                "h-40 w-full rounded-md bg-gray-300",
                 "dark:bg-gray-200",
               )}
             />
@@ -38,19 +38,19 @@ export default function Blog() {
           <div className="flex w-full flex-col gap-2">
             <div
               className={clsx(
-                "h-5 w-full max-w-60 animate-pulse rounded-md bg-gray-300",
+                "h-5 w-full max-w-60 rounded-md bg-gray-300",
                 "dark:bg-gray-200",
               )}
             />
             <div
               className={clsx(
-                "h-3 w-full max-w-40 animate-pulse rounded-md bg-gray-300",
+                "h-3 w-full max-w-40 rounded-md bg-gray-300",
                 "dark:bg-gray-200",
               )}
             />
             <div
               className={clsx(
-                "h-40 w-full animate-pulse rounded-md bg-gray-300",
+                "h-40 w-full rounded-md bg-gray-300",
                 "dark:bg-gray-200",
               )}
             />
